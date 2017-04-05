8d87c48a32f2689d264723fb4fc41cdb8691ed55
Julian Herr <mail@julianherr.de>
2017-04-05 09:30:30 +0200

    Add initial src/main.rs

---
+++ src/main.rs
@@ -0,0 +1,71 @@
+#![no_std]
+#![no_main]
+
+#![macro_use]
+extern crate stm32f7_discovery as stm32f7;
+extern crate r0;
+
+use stm32f7::{system_clock, board, embedded};
+
+#[no_mangle]
+pub unsafe extern "C" fn reset() -> ! {
+
+    extern "C" {
+        static __DATA_LOAD: u32;
+        static __DATA_END: u32;
+        static mut __DATA_START: u32;
+
+        static mut __BSS_START: u32;
+        static mut __BSS_END: u32;
+    }
+
+    let data_load = &__DATA_LOAD;
+    let data_start = &mut __DATA_START;
+    let data_end = &__DATA_END;
+
+    let bss_start = &mut __BSS_START;
+    let bss_end = &__BSS_END;
+
+    // initializes the .data section (copy the data segment initializers from flash to RAM)
+    r0::init_data(data_start, data_end, data_load);
+    // zeroes the .bss section
+    r0::zero_bss(bss_start, bss_end);
+
+    stm32f7::heap::init();
+
+    main(board::hw());
+}
+
+
+fn main(hw: board::Hardware) -> ! {
+
+let board::Hardware {
+        rcc,
+        pwr,
+        flash,
+        fmc,
+        ltdc,
+        gpio_a,
+        gpio_b,
+        gpio_c,
+        gpio_d,
+        gpio_e,
+        gpio_f,
+        gpio_g,
+        gpio_h,
+        gpio_i,
+        gpio_j,
+        gpio_k,
+        i2c_3,
+        sai_2,
+        syscfg,
+        ethernet_mac,
+        ethernet_dma,
+        ..
+    } = hw;
+
+
+    loop {
+
+    }
+}
